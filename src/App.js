83092e54b2cb0ccd537d7a4c461dddc54da0ef3e
P.P.S.JAYANTH REDDY <ppsjreddy85@gmail.com>
2023-11-10 22:14:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WordCounter from './Components/countWord';
+function App() {
+  return (
+    <div className="App">
+      <h1>Responsive Paragraph Word Counter</h1>
+      <WordCounter/> 
+    </div>
+  );
+}
+
+export default App;
